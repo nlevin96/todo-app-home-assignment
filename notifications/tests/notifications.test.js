1b063bd189fce8f4881d8fde19fe9a2706634aff
nlevin96 <natalilevin72@gmail.com>
2023-04-15 16:03:49 +0300

    unit test added to notifications

---
+++ notifications/tests/notifications.test.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+const scheduleNotifications = require('../services/notifications');
+
+// Mock axios.get
+jest.mock('axios');
+axios.get.mockResolvedValue({
+  data: [
+    { content: 'Task 1', deadline: '2023-04-20' },
+    { content: 'Task 2', deadline: '2023-04-20' }
+  ]
+});
+
+describe('schedule notifications', () => {
+  beforeEach(() => {
+    jest.useFakeTimers('modern');
+    scheduleNotifications()
+  });
+
+  afterEach(() => {
+    jest.runOnlyPendingTimers();
+    jest.clearAllMocks();
+    jest.useRealTimers();
+  });
+
+  it('should send notifications every 5 minutes', async () => {
+    const consoleSpy = jest.spyOn(global.console, 'log');
+
+    jest.advanceTimersByTime(5 * 60 * 1000);
+    setImmediate(async () => {
+        expect(axios.get).toHaveBeenCalled();
+        expect(consoleSpy).toHaveBeenCalledTimes(3);
+        expect(consoleSpy).toHaveBeenCalledWith("Notification sent for the following task: Task 1 | deadline 2023-04-20");
+        expect(consoleSpy).toHaveBeenNthCalledWith(2, "Notification sent for the following task: Task 2 | deadline 2023-04-20");
+        expect(consoleSpy).toHaveBeenLastCalledWith('------------------------------------------');
+        jest.clearAllMocks();
+      });
+  });
+});
